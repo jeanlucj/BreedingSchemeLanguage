c914ec7d55fa727344ae54045a3ad69248694707
Jean-Luc Jannink <jeanluc.work@gmail.com>
2017-01-18 08:06:51 -0500

    Streamlined the function for calculating genotypic values

--- R/calculationFunc.R
+++ R/calculationFunc.R
@@ -3,30 +3,29 @@
 #'@param geno matrix of haplotypes
 #'@param mapData map data
 #'
+# Calculate genotypic value one QTL at a time for all individuals
 calcGenotypicValue <- function(geno, mapData){
-  nPop <- nrow(geno) / 2
-  gv1pos <- function(geno1pos, actType, effect){
-    geno1pos <- as.matrix(geno1pos, nrow = 2)
+  nInd <- nrow(geno) / 2
+  # Calculate the contribution to genotypic value of one indiviudal of one QTL
+  gv1indThisQ <- function(ind){ # ind is zero base
+    geno1pos <- geno[ind*2 + 1:2, posThisQ, drop=F]
     if (!is.null(mapData$domModel)){ # 1 dominant over -1; degree in actType
-      mnMxGeno <- apply(geno1pos, 2, range)
-      sapply(1:length(actType), function(i) c(1-actType[i], actType[i]) %*% mnMxGeno[,i])
+      minMaxGeno <- apply(geno1pos, 2, range)
+      coef <- sapply(1:length(actType), function(i) c(1-actType[i], actType[i]) %*% minMaxGeno[,i])
     } else{ # Standard model
       coef <- ifelse(actType == 0, (geno1pos[1, ] + geno1pos[2, ])/2, -(geno1pos[1, ] * geno1pos[2, ]))
     }
     return(effect * prod(coef))
   }
-  gv1ind <- function(genoVec, mapData){
-    nQTL <- max(mapData$effectID)
-    geno1ind <- rbind(genoVec[1:(length(genoVec) / 2)], genoVec[(length(genoVec) / 2 + 1):length(genoVec)])
-    power <- 0
-    for(i in 1:nQTL){
-      power <- power + gv1pos(geno = geno1ind[, mapData$effectivePos[mapData$effectID == i]], actType = mapData$actionType[mapData$effectID == i], effect = mapData$effects[i, 1])
-    }
-    return(power)
+
+  genoVal <- numeric(nInd)
+  for(i in 1:nQTL){
+    posThisQ <- mapData$effectivePos[mapData$effectID == i]
+    actType <- mapData$actionType[mapData$effectID == i]
+    effect <- mapData$effects[i, 1]
+    genoVal <- genoVal + sapply(0:(nInd - 1), gv1indThisQ)
   }
-  genoVec <- cbind(geno[1:nPop * 2 - 1, ], geno[1:nPop * 2, ])
-  gv <- apply(genoVec, 1, gv1ind, mapData = mapData)
-  return(gv)
+  return(genoVal)
 }
 
 #'calcPhenotypicValue
