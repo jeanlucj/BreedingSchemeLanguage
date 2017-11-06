6b3eb6e722a8e4fcd83b2efe8ac6912688a03b1e
Jean-Luc Jannink <jeanluc.work@gmail.com>
2017-11-06 10:43:24 -0500

    Removed a bug that I don't fully understand but had to do with the popID becoming a global value and therefore staying at zero rather than being the latest population

--- R/predictValue.R
+++ R/predictValue.R
@@ -15,29 +15,13 @@
 predictValue <- function(sEnv=simEnv, popID=NULL, trainingPopID=NULL, locations=NULL, years=NULL, sharingInfo=NULL){
   parent.env(sEnv) <- environment()
   predictValue.func <- function(bsl, popID, trainingPopID, locations, years, sharingInfo){
+    if (is.null(popID)) popID <- max(bsl$genoRec$popID)
+    if (is.null(sharingInfo)) sharingInfo <- bsl$selCriterion$sharing
     if (is.null(locations)) locations <- unique(bsl$phenoRec$loc)
     if (is.null(years)) years <- unique(bsl$phenoRec$year)
     phenoRec <- subset(bsl$phenoRec, subset = bsl$phenoRec$loc %in% locations & bsl$phenoRec$year %in% years)
     ########################################################
-    # Consider all individuals to be IID
-    if (sharingInfo == "none"){
-      mt1ObsPerGID <- length(unique(phenoRec$phenoGID)) < nrow(phenoRec)
-      if (mt1ObsPerGID){ # More than one observation per GID: run a model
-        fmla <- "pValue ~ (1|phenoGID)"
-        if (length(unique(phenoRec$year))) fmla <- paste(fmla, "+ year + (1|phenoGID:year)")
-        if (length(unique(phenoRec$loc))) fmla <- paste(fmla, "+ loc + (1|phenoGID:loc)")
-        phenoRec$phenoGID <- factor(phenoRec$phenoGID)
-        phenoRec$loc <- factor(phenoRec$loc)
-        phenoRec$year <- factor(phenoRec$year)
-        fitIID <- lmer(formula=as.formula(fmla), data=phenoRec, weights=1/phenoRec$error)
-        predict <- ranef(fitIID)$phenoGID
-        predGID <- as.numeric(rownames(predict))
-      } else{ # Only one observation per GID
-        predict <- phenoRec$pValue
-        predGID <- as.numeric(phenoRec$phenoGID)
-      }
-    }
-    
+
     # Use kin.blup4.4 that has reduce and R features
     ########################################
     kin.blup4.4 <- function(data,geno,pheno,GAUSS=FALSE,K=NULL,fixed=NULL,covariate=NULL,PEV=FALSE,n.core=1,theta.seq=NULL,reduce=FALSE,R=NULL)  {
@@ -206,15 +190,27 @@ predictValue <- function(sEnv=simEnv, popID=NULL, trainingPopID=NULL, locations=
             } #else GAUSS
         } #else is.null(K)
     } #kin.blup
-    ##########################
     
+    ########################################################
+    # Consider all individuals to be IID
+    if (sharingInfo == "none"){
+      # Figure out who to predict
+      tf <- bsl$genoRec$popID %in% popID
+      predGID <- bsl$genoRec$GID[tf]
+      mt1ObsPerGID <- sum(phenoRec$phenoGID %in% predGID) > length(predGID)
+      
+      kbDat <- subset(phenoRec, phenoRec$phenoGID %in% predGID)
+      kbo <- kin.blup4.4(kbDat, geno="phenoGID", pheno="pValue", fixed=c("loc", "year"), reduce=mt1ObsPerGID, R=kbDat$error)
+      predict <- kbo$g
+      predGID <- as.integer(names(predict))
+    }
     ########################################################
     # Use markers to determine individual relatedness
     if (sharingInfo == "markers"){
       trainCandidates <- bsl$genoRec$hasGeno & bsl$genoRec$GID %in% phenoRec$phenoGID
-      if(is.null(trainingPopID)){
+      if (is.null(trainingPopID)){
         GID.train <- bsl$genoRec$GID[trainCandidates]
-      }else{
+      } else{
         tf <- bsl$genoRec$popID %in% trainingPopID
         GID.train <- bsl$genoRec$GID[tf & trainCandidates]
       }
@@ -222,7 +218,6 @@ predictValue <- function(sEnv=simEnv, popID=NULL, trainingPopID=NULL, locations=
       M <- (bsl$geno[GID.train*2 - 1, mrkPos] + bsl$geno[GID.train*2, mrkPos]) / 2
       
       # Figure out who to predict
-      if (is.null(popID)) popID <- max(bsl$genoRec$popID)
       tf <- bsl$genoRec$popID %in% popID
       GID.pred <- setdiff(bsl$genoRec$GID[tf], GID.train)
       M <- rbind(M, (bsl$geno[GID.pred*2 - 1, mrkPos] + bsl$geno[GID.pred*2, mrkPos]) / 2)
@@ -234,6 +229,7 @@ predictValue <- function(sEnv=simEnv, popID=NULL, trainingPopID=NULL, locations=
       kbDat <- subset(phenoRec, phenoRec$phenoGID %in% predGID)
       kbo <- kin.blup4.4(kbDat, geno="phenoGID", pheno="pValue", fixed=c("loc", "year"), K=K, reduce=mt1ObsPerGID, R=kbDat$error)
       predict <- kbo$g
+      predGID <- as.integer(names(predict))
     }
     ########################################################
     # Use pedigree to determine individual relatedness
@@ -247,7 +243,6 @@ predictValue <- function(sEnv=simEnv, popID=NULL, trainingPopID=NULL, locations=
         tf <- bsl$genoRec$popID %in% trainingPopID
         GID.train <- bsl$genoRec$GID[tf & trainCandidates]
       }
-      if (is.null(popID)) popID <- max(bsl$genoRec$popID)
       tf <- bsl$genoRec$popID %in% popID
       GID.pred <- setdiff(bsl$genoRec$GID[tf], GID.train)
       predGID <- c(GID.train, GID.pred)
@@ -258,6 +253,7 @@ predictValue <- function(sEnv=simEnv, popID=NULL, trainingPopID=NULL, locations=
       kbDat <- subset(phenoRec, phenoRec$phenoGID %in% predGID)
       kbo <- kin.blup4.4(kbDat, geno="phenoGID", pheno="pValue", fixed=c("loc", "year"), K=K, reduce=mt1ObsPerGID, R=kbDat$error)
       predict <- kbo$g
+      predGID <- as.integer(names(predict))
     }
         
     if(is.null(bsl$predRec)){
@@ -273,8 +269,8 @@ predictValue <- function(sEnv=simEnv, popID=NULL, trainingPopID=NULL, locations=
     if (exists("totalCost", bsl)) bsl$totalCost <- bsl$totalCost + bsl$costs$predCost
     return(bsl)
   }#END predict.func
+  
   with(sEnv, {
-    if (is.null(sharingInfo)) sharingInfo <- sims[[1]]$selCriterion$sharing
     if(nCore > 1){
       sfInit(parallel=T, cpus=nCore)
       sims <- sfLapply(sims, predictValue.func, popID=popID, trainingPopID=trainingPopID, locations=locations, years=years, sharingInfo=sharingInfo)
